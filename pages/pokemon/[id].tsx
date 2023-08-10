ebd4500f310597c371855a0cf028b89881224318
osorioadrian <osorioadriancamilo@gmail.com>
2023-08-09 19:31:03 -0500

    incremental static regeneration ISR

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -102,7 +102,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemons151.map( id => ({
       params: { id }
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -110,10 +110,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const { id } = params as { id: string };
 
+  const pokemon = await getPokemonInfo( id );
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo( id )
-    }
+      pokemon
+    },
+    revalidate: 86400
   }
 }
 
